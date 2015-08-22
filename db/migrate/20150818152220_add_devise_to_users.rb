8ca7dd98bbb443ae36969d09b1af34bc9e6dc904
Mike Kavouras <kavourasm@gmail.com>
2015-08-22 14:46:56 -0400

    updated user settings page

--- db/migrate/20150818152220_add_devise_to_users.rb
+++ db/migrate/20150818152220_add_devise_to_users.rb
@@ -44,7 +44,6 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    remove_column :users, :password_digest
     raise ActiveRecord::IrreversibleMigration
   end
 end
